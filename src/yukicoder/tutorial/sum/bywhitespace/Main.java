253df98920748e7cc19ed044137e2484fa79a261
cosmo <cosmo@192.168.10.110>
2020-05-19 17:49:32 +0900

    Tutorial sum by whitespace

---
+++ src/yukicoder/tutorial/sum/bywhitespace/Main.java
@@ -0,0 +1,38 @@
+package yukicoder.tutorial.sum.bywhitespace;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		new Main().execute();
+
+	}
+
+	private void execute() {
+		List<Long> nums = read();
+
+		long sum = 0L;
+
+		for (Long num : nums) {
+			sum += num;
+		}
+
+		System.out.println(sum);
+	}
+
+	private List<Long> read() {
+		@SuppressWarnings("resource")
+		Scanner sc = new Scanner(System.in);
+		int length = sc.nextInt();
+
+		List<Long> numsList = new ArrayList<>();
+		for (int i = 0; i < length; i++) {
+			numsList.add(sc.nextLong());
+		}
+		return numsList;
+	}
+
+}
