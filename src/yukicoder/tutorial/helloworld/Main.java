517346ad49f43c532ad8ed6d40ed41cbe56fac9d
cosmo <cosmo@192.168.10.110>
2020-05-19 16:59:07 +0900

    Implement Hello World

---
+++ src/yukicoder/tutorial/helloworld/Main.java
@@ -0,0 +1,12 @@
+package yukicoder.tutorial.helloworld;
+
+public class Main {
+
+	public static void main(String[] args) {
+		new Main().execute();
+	}
+
+	private void execute() {
+		System.out.println("Hello World!");
+	}
+}
