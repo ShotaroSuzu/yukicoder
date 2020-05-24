4be3d934f6e263d93a566ad5ea2e00858dd582a9
cosmo <cosmo@192.168.10.110>
2020-05-24 18:25:51 +0900

    Level2 Bit Sugoroku AC answer

--- src/yukicoder/level2/bitsugoroku/BitSugorokuRecursive.java
+++ src/yukicoder/level2/bitsugoroku/BitSugorokuRecursive.java
@@ -1,141 +1,99 @@
 package yukicoder.level2.bitsugoroku;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
+import java.util.HashSet;
 import java.util.Scanner;
+import java.util.Set;
 
 public class BitSugorokuRecursive {
 
-	private static final int START_POINT = 1;
-	private int goalNumber;
-	
+	private Integer goal;
+
 	public static void main(String[] args) {
 		new BitSugorokuRecursive().execute();
 	}
 
 	private void execute() {
-		// やること
-		//1.入力する数字の一つ前になりうる点をあげる
-		//2.そのうち、実際に来れるやつを列挙し、[自分,列挙1][自分,列挙2]という経路を作る
-		//4.「列挙が一つもない場合は、「列挙」は到達不能なので自分自身を含む配列を消す
-		//5.列挙に自身が属する配列に一度でも出てきた数があれば自分自身を含む配列を消す（無限ループにならないように）
-		//3.この操作を列挙1,列挙2について「列挙」が1になるまで繰り返す
-		//6.１までたどり着いたもののうち、最も個数が少ないものの経路数を数える
-		
-		goalNumber = read();
-		List<Integer> initialRoute = Arrays.asList(goalNumber);
-		List<List<Integer>> routes = new ArrayList<>();
-		routes.add(initialRoute);
-		
-		
-		List<List<Integer>> fullRoutes = new ArrayList<>();
-		traceRoutesRecursively(routes, fullRoutes);
-		
-		output(getShortestRouteSize(fullRoutes));
+		goal = read();
+
+		Integer firstLocation = 1;
+		int minimumMovingNum = calcMinimanMovingNumber(firstLocation);
+
+		output(minimumMovingNum);
+	}
+
+	private int calcMinimanMovingNumber(Integer firstLocation) {
+		Set<Integer> presentLocations = new HashSet<>();
+		presentLocations.add(firstLocation);
+
+		Set<Integer> pastLocations = new HashSet<>();
+		pastLocations.add(firstLocation);
+
+		return goToNextLocation(pastLocations, presentLocations, 1);
+	}
+
+	private int goToNextLocation(Set<Integer> pastLocations, Set<Integer> presentLocations, int count) {
+		if(presentLocations.isEmpty()) {
+			return -1;
+		}
+		if(presentLocations.contains(goal)) {
+			return count;
+		}
+
+		Set<Integer> newLocations = new HashSet<Integer>();
+		for (Integer presentLocation : presentLocations) {
+			int movingDistance = calcMovingDistance(presentLocation);
+
+			Integer nextForwardLocation = presentLocation + movingDistance;
+			if(canMoveTo(nextForwardLocation, pastLocations)) {
+				newLocations.add(nextForwardLocation);
+				pastLocations.add(nextForwardLocation);
+			}
+			
+			Integer nextBackwordLocation = presentLocation - movingDistance;
+			if(canMoveTo(nextBackwordLocation, pastLocations)) {
+				newLocations.add(nextBackwordLocation);
+				pastLocations.add(nextBackwordLocation);
+			}
+		}
+
+		return goToNextLocation(pastLocations, newLocations, ++count);
+	}
+
+	private boolean canMoveTo(Integer location,Set<Integer> pastNumbers) {
+		return 1 <= location && location <= goal
+				&& pastNumbers.contains(location) == false;
+	}
+
+	/**
+	 * Moving distance is defined as the number of "1"s of the "location" represented in binary.
+	 * This method calc the number of "1"s of the "location" represented in binary.
+	 * @param location
+	 * @return the number of "1"s.
+	 */
+	private int calcMovingDistance(Integer location) {
+		return countOneNumberInBinaly(location, 0);
+	}
+
+	private Integer countOneNumberInBinaly(Integer decimal, int count) {
+		if(decimal < 1) {
+			return count;
+		}
+		int base = 2;
+		int digit = 1;
+		while(digit * base <= decimal) {
+			digit *= base;
+		}
+		Integer newDecimal = decimal - digit;
+		return countOneNumberInBinaly(newDecimal, ++count);
 	}
 
 	private void output(int size) {
 		System.out.println(size);
 	}
 
-	private int getShortestRouteSize(List<List<Integer>> routes) {
-		int shortestRouteSize = Integer.MAX_VALUE;
-		for (List<Integer> route : routes) {
-			if(shortestRouteSize > route.size()) {
-				shortestRouteSize = route.size();
-			}
-		}
-		
-		if(shortestRouteSize == Integer.MAX_VALUE) {
-			return -1;
-		}
-		return shortestRouteSize;
-	}
-
-	private void traceRoutesRecursively(List<List<Integer>> routes, List<List<Integer>> fullRoutes) {
-		for (List<Integer> route : routes) {
-			Integer lastNum = route.get(route.size() - 1);
-			List<Integer> probablePrevNumbers = getProbalbePrevNumbers(lastNum);
-			
-			if(isStartPoint(probablePrevNumbers)) {
-				route.add(probablePrevNumbers.get(0));
-				fullRoutes.add(route);
-				continue;
-			}
-			
-			List<List<Integer>> newRoutes = new ArrayList<>();
-			for (Integer probableNumber : probablePrevNumbers) {
-				if(route.contains(probableNumber)) {
-					continue;
-				}
-				
-				List <Integer> newRoute = new ArrayList<Integer>(route);
-				newRoute.add(probableNumber);
-				newRoutes.add(newRoute);
-			}
-			traceRoutesRecursively(newRoutes, fullRoutes);
-		}
-	}
-
-	private boolean isStartPoint(List<Integer> probableNumbers) {
-		return probableNumbers.isEmpty() == false && probableNumbers.size() == 1 && probableNumbers.get(0) == START_POINT; 
-	}
-
-	private List<Integer> getProbalbePrevNumbers(int distNum) {
-		int probableRange = calcProbableRange(distNum);
-		
-		List<Integer> probableNumbers = new ArrayList<>();
-		
-		probableNumbers.addAll(getBackwordProbableNumbers(distNum, probableRange));
-		probableNumbers.addAll(getForwardProbableNumbers(distNum, probableRange));
-		
-		return probableNumbers;
-	}
-	
-	private List<Integer> getBackwordProbableNumbers(int distNum, int range) {
-		List<Integer> backwordProbableNumbers = new ArrayList<>();
-		for(int candidate = (distNum - range) ; candidate < distNum; candidate++) {
-			int forwardNumber = calcForwardOrBackwardNumber(candidate);
-			if(candidate + forwardNumber == distNum) {
-				backwordProbableNumbers.add(Integer.valueOf(candidate));
-			}
-		}
-		return backwordProbableNumbers;
-	}
-	
-	private List<Integer> getForwardProbableNumbers(int distNum, int range) {
-		List<Integer> forwardProbableNumbers = new ArrayList<>();
-		for(int candidate = distNum + 1 ; candidate <= distNum + range; candidate++) {
-			if(candidate > goalNumber) {
-				continue;
-			}
-			int forwardNumber = calcForwardOrBackwardNumber(candidate);
-			if(candidate - forwardNumber == distNum) {
-				forwardProbableNumbers.add(Integer.valueOf(candidate));
-			}
-		}
-		return forwardProbableNumbers;
-	}
-	
-	private int calcForwardOrBackwardNumber(Integer num) {
-		char[] numOfBynary = Integer.toBinaryString(num).toCharArray();
-		
-		Arrays.sort(numOfBynary);
-		String numOfBynaryString = new String(numOfBynary);
-		
-		return numOfBynary.length - (numOfBynaryString.indexOf("1"));
-	}
-
-	private int calcProbableRange(Integer num) {
-		return Integer.toBinaryString(num).length() - 1;
-	}
 	private int read() {
 		@SuppressWarnings("resource")
 		Scanner sc = new Scanner(System.in);
 		return sc.nextInt();
 	}
-
-
-
 }
